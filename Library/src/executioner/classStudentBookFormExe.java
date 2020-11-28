99994650d4ab56add60083d13a4c2d7290f13144
spitzc32 <33305108+spitzc32@users.noreply.github.com>
2020-11-28 16:23:50 +0800

    Updated Relational Entity Example

---
+++ Library/src/executioner/classStudentBookFormExe.java
@@ -0,0 +1,85 @@
+package executioner;
+
+import SQLCrudStatements.classAssociate_StudentMaterialTrackerCRUD;
+import values.classEntity;
+import values.classObject;
+import values.classAssociate_StudentMaterialTracker;
+import general.classComboItem;
+
+import java.sql.Date;
+import java.util.ArrayList;
+
+import javax.swing.JComboBox;
+import javax.swing.table.DefaultTableModel;
+
+public class classStudentBookFormExe {
+	
+	public static void funcRetrieveEntityVal (String table, JComboBox<classComboItem> comboBox) {
+		ArrayList<classEntity> entities = classAssociate_StudentMaterialTrackerCRUD.funcRetrieveAssociate_StudentMaterialTrackerObj(table);
+		
+		comboBox.addItem(new classComboItem("select", 0));
+		
+		for (classEntity entity: entities) {
+			String name = entity.funcgetLastName() + ", " + 
+						  entity.funcgetFirstName() + " " +
+						  entity.funcgetMiddleName();
+			comboBox.addItem(new classComboItem(name, entity.funcgetId()));
+		}
+	}
+	
+	public static void funcRetrieveObjectVal (String table, JComboBox<classComboItem> comboBox) {
+		ArrayList<classObject> objects = classAssociate_StudentMaterialTrackerCRUD.funcRetrieveObjectAssociate_StudentMaterialTrackerObj(table);
+		
+		comboBox.addItem(new classComboItem("select", 0));
+		
+		for (classObject object: objects) {
+			comboBox.addItem(new classComboItem(object.funcgetTitle(), object.funcgetId()));
+		}
+	}
+	
+	public static void setValues(classAssociate_StudentMaterialTracker student, int studentId, int subjectId, int librarianId, int materialId, 
+			Date dateBorrowed, Date dateReturned, int isDue, int isReturned, Date assignedDateReturn) {
+		student.funcsetStudentId(studentId);
+		student.funcsetSubjectId(subjectId);
+		student.funcsetlibririanId(librarianId);
+		student.funcsetMaterialId(materialId);
+		student.funcsetDateBorrowed(dateBorrowed);
+		student.funcsetDateReturned(dateReturned);
+		student.funcsetIsDue(isDue);
+		student.funcsetIsReturned(isReturned);
+		student.funcsetAssignedDateReturn(assignedDateReturn);
+	}
+	
+	public static String exeInsertStatements (classAssociate_StudentMaterialTracker student) {
+		int intRes = classAssociate_StudentMaterialTrackerCRUD.funcCreateAssociate_StudentMaterialTrackerObj(student);
+		
+		if (intRes > 0) {
+			return "Saved Changes. Please exit then reload this window.";
+		} else {
+			return "Changes failed to reflect in the database. Try Again";
+		}
+	}
+	
+	public static void exeReadStatements (DefaultTableModel table) {
+		/* TODO: need an execute statement that returns a list of values so that we can
+		 * incorporate the values returned from our statement to this table.  
+		 * */
+		ArrayList<classAssociate_StudentMaterialTracker> students =  classAssociate_StudentMaterialTrackerCRUD.funcReadAssociate_StudentMaterialTrackerObj(); 
+		
+		for (classAssociate_StudentMaterialTracker student : students) {
+			Object objList[] = {
+								student.funcgetId(),
+								student.funcgetSubjectId(), 
+								student.funcgetlibririanId(),
+								student.funcgetMaterialId(),
+								student.funcgetDateBorrowed(),
+								student.funcgetDateReturned(),
+								student.funcgetIsDue() == 1 ? "Yes" : "No",
+								student.funcgetIsReturned() == 1 ? "Yes" : "No",
+								student.funcgetAssignedDateReturn()
+							   };
+			table.addRow(objList);
+		}
+	}
+	
+}

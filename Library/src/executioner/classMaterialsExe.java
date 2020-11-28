2148ddcfa05ae05eb1fd4ac1e06e8298f3360feb
kibalias <74294078+kibalias@users.noreply.github.com>
2020-11-28 12:55:02 +0800

    Added classMaterialsExe.java

---
+++ Library/src/executioner/classMaterialsExe.java
@@ -0,0 +1,54 @@
+package executioner;
+
+import java.sql.Date;
+import values.classMaterials;
+import values.classStudent;
+import SQLCrudStatements.classMaterialsCRUD;
+import SQLCrudStatements.classStudentCRUD;
+
+import javax.swing.table.DefaultTableModel;
+
+import java.util.ArrayList;
+
+public class classMaterialsExe {
+	public static void setValues(classMaterials mat, String MaterialTitle, String Description, String Edition,
+			Date YearOfPublication, Date DatePublished, String TypeOfMaterial) {
+		mat.funcsetTitle(MaterialTitle);
+		mat.funcsetDescription(Description);
+		mat.funcsetEdition(Edition);
+		mat.funcsetYearOfPublication(YearOfPublication);
+		mat.funcsetDatePublished(DatePublished);
+		mat.funcsetTypeOfMaterial(TypeOfMaterial);
+	}
+	
+	public static String exeInsertStatements (classMaterials mat) {
+		int intRes = classMaterialsCRUD.funcCreateMaterialsObj(mat);
+		
+		if (intRes > 0) {
+			return "Saved Changes. Please exit then reload this window.";
+		} else {
+			return "Changes failed to reflect in the database. Try Again";
+		}
+	}
+	
+	public static void exeReadStatements (DefaultTableModel table) {
+		/* TODO: need an execute statement that returns a list of values so that we can
+		 * incorporate the values returned from our statement to this table.  
+		 * */
+		ArrayList<classMaterials> materials =  classMaterialsCRUD.funcReadMeterialsObj(); 
+		
+		for (classMaterials material : materials) {
+			Object objList[] = {
+								material.funcgetId(),
+								material.funcgetTitle(),
+								material.funcgetDescription(), 
+								material.funcgetEdition(),
+								material.funcgetYearOfPublication(),
+								material.funcgetDatePublished(),
+								material.funcgetTypeOfMaterial(),
+							
+							   };
+			table.addRow(objList);
+		}
+	}
+}
